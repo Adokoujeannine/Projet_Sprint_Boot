722a23dd12c24a1d05c88c9185df17d08bdb56f5
Adokoujeannine <jeannineadokou@gmail.com>
2022-09-30 17:34:33 +0200

    first commit

--- src/main/java/com/example/demo/personne/PersonneController.java
+++ src/main/java/com/example/demo/personne/PersonneController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -42,5 +44,17 @@ public class PersonneController {
     @DeleteMapping(path = "{PersonneId}")
     public void deletePersonne(@PathVariable ("PersonneId") Long PersonneId){
         PersonneService.deletePersonne(PersonneId);
+    }
+    
+      @PutMapping(path = "{PersonneId}")  
+    public void updatePersonne(
+                                @PathVariable ("PersonneId") Long PersonneId,
+                                @RequestParam (required = false) String nom,
+                                @RequestParam (required = false) String prenom,
+                                @RequestParam (required = false) String email){
+        PersonneService.updatePersonne(PersonneId, nom, prenom, email);
+    }
+            {
+        
     }
 }
